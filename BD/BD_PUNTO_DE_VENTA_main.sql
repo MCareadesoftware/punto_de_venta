1eab8dcd510122e5e031be82e6497aab2cd528b0
Javier Ayllon <programador.monstruocreativo@gmail.com>
2023-02-20 12:10:28 -0500

    Elaboración de la BD para el punto de venta

---
+++ BD/BD_PUNTO_DE_VENTA_main.sql
@@ -0,0 +1,9 @@
+-----------------------------------------------------------
+-- Empresa        :  Monstruo Creativo
+-- Software       :  Sistema de Punto de Venta
+-- DBMS           :  MS SQL Server
+-- Base de Datos  :  BD_PUNTO_DE_VENTA
+-- Script         :  Crea la base de datos BD_PUNTO_DE_VENTA
+-- Programado por :  Equipo de Software Monstruo Creativo
+---------------------------------------------------------
+
